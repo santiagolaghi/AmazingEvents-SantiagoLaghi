0a74f4077ddc3f1def0d244a879ffe9389d93d24
SANTIAGO LAGHI <santiagonlaghi@hotamil.com>
2023-08-04 02:17:59 -0300

    Sprint2terminado

--- assets/scripts/UpComingEvents.js
+++ assets/scripts/UpComingEvents.js
@@ -2,17 +2,17 @@ const contenedorTarjetas = document.getElementById ('contenedorTarjetas');
 const eventos = data.events;
 
 function crearTarjeta (objeto) {        
-    return  `<div class="card" style="width: 18rem;">         
-            <img class="image" src="${objeto.image}">
-            <div class="card-body">
-            <h5 class="card-title">${objeto.name}</h5>
-            <p class="card-text">${objeto.description}</p>
-            <div class="d-flex justify-content-evenly align-items-center pt-5">
-            <p class="card-text">${objeto.price}</p>
-            <a href="./assets/pages/Details.html" class="btn btn-primary">Details</a>
-            </div>                  
-            </div>
-            </div>`        
+    return   `<div class="card" style="width: 18rem;">         
+                <img class="imagecard" src="${objeto.image}">
+                <div class="card-body d-flex flex-column align-items-center text-center">
+                    <h5 class="card-title">${objeto.name}</h5>
+                    <p class="card-text">${objeto.description}</p>
+                    <div class="divpya d-flex justify-content-around align-items-center pt-4">                       
+                        <p class="cardtextp card-text">$${objeto.price}</p>
+                        <a class="botona text-decoration-none border border-2 p-1" href="./assets/pages/Details.html" class="btn btn-primary">Details</a>
+                    </div>                  
+                </div>
+            </div>`       
 }
 
 function mostrarTarjeta (listaDeEventos){
